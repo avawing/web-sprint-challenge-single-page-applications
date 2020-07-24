1466f11c8764e1c346d618aaf95a4c1a5babe3c2
Aveline Wingfield <avelinewingfield@gmail.com>
2020-07-24 14:51:38 -0400

    So many problems

--- src/components/Order.js
+++ src/components/Order.js
@@ -7,7 +7,8 @@ export default function Order(props){
         <>
     <h2>{order.name}</h2>
     <h3>{order.email}</h3>
-    <h3>{order.slice}{order.toppings}</h3>
+    <h3>{order.slice}</h3>
+    <h4>{order.instructions}</h4>
     </>
     
     )
